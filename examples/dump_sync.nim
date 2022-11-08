1bc3a5db10d27760b782ba1dcc2f14968a9079dd
inv2004 <inv2004@gmail.com>
2022-11-09 02:51:18 +0300

    attrs in dump_sync

--- examples/dump_sync.nim
+++ examples/dump_sync.nim
@@ -10,12 +10,11 @@ let ld = newLdap host
 ld.saslBind login, pass
 echo ld.whoAmI()
 
-for entry in ld.search("(objectclass=*)", @[""], LdapScope.Base, ""):
+for entry in ld.search("(objectclass=*)", ["+", "*"], LdapScope.Base, ""):
   echo entry.getDN()
   for attr, vals in entry:
     echo fmt"""  {attr} ({vals.len}): {(vals.values().join", ")}"""
 
-# for entry in ld.search("(objectclass=*)", ctrls = @[newCtrl Extension.LDAP_SERVER_NOTIFICATION_OID]):
 for entry in ld.search "(objectclass=*)":
   echo entry.getDN()
   for attr, vals in entry:
